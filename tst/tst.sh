d9e8cfc14b17178035f7230e6775a10cc56879ac
anarticho <ptrkrpkne@gmail.com>
2024-07-22 18:59:43 +0200

    - Change make process (test)

--- tst/tst.sh
+++ tst/tst.sh
@@ -13,33 +13,35 @@ PRM="tst/prm.bin"
 XVL="xvl.bin"
 ESC="esc.bin"
 
-
 # Execute binject with first set of parameters
 build/binject -of $T1F -s ABCD -x 45464748 -ns 4 I -nx 4 4A -a 12 -na 2 ACAB1312DEADBEEF
 
 # Check if tst1.bin matches _vec.bin
+printf "[*] > binject -of tst1.bin -s ABCD -x 45464748 -ns 4 I -nx 4 4A -a 12 -na 2 ACAB1312DEADBEEF\n"
 if cmp -s $VCF $T1F
 then : 
-    printf "[*] Test 1:\tOK\n"
+    printf "[*] > OK\n[*]\n"
     # Execute binary with second set of parameters
-    build/binject -if $T1F > $T2F    
+    build/binject -if $T1F > $T2F  
+    printf "[*] > binject -if tst1.bin > tst2.bin\n"  
     if cmp -s $VCF $T2F
     then : 
-        printf "[*] Test 2:\tOK\n"
+        printf "[*] > OK\n[*]\n"
         # Execute -bd flag
         build/binject -bd $BAD $PRM > /dev/null
+        printf "[*] > binject -bd bad.bin prm.bin\n"
         if cmp -s $ESC $T3F
         then :
-            printf "[*] Test 3:\tOK\n\n"
+            printf "[*] > OK\n[*]\n"
             printf "[*] Check succeed\n"
         else :
-            printf "[*] Test 3:\tFailed\n"
+            printf "[*] > FAIL\n[*]\n"
         fi
     else :
-        printf "[*] Test 2:\tFailed\n"
+        printf "[*] > FAIL\n[*]\n"
     fi
 else :
-    printf "[*] Test 1:\tFailed\n"
+    printf "[*] > FAIL\n[*]\n"
 fi
 rm -f $T1F
 rm -f $T2F
