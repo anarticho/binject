c8baf35daeac8ae65a37c3cc4b4b32dbdede2980
anarticho <ptrkrpkne@gmail.com>
2024-07-22 07:15:09 +0200

    - Test for -bd flag

--- tst/tst.sh
+++ tst/tst.sh
@@ -5,6 +5,14 @@
 VCF="tst/tst.bin"
 T1F="tst/tst1.bin"
 T2F="tst/tst2.bin"
+T3F="tst/tst3.bin"
+
+BAD="tst/bad.bin"
+PRM="tst/prm.bin"
+
+XVL="xvl.bin"
+ESC="esc.bin"
+
 
 # Execute binject with first set of parameters
 build/binject -of $T1F -s ABCD -x 45464748 -ns 4 I -nx 4 4A -a 12 -na 2 ACAB1312DEADBEEF
@@ -17,8 +25,16 @@ then :
     build/binject -if $T1F > $T2F    
     if cmp -s $VCF $T2F
     then : 
-        printf "[*] Test 2:\tOK\n\n"
-        printf "[*] Check succeed\n"
+        printf "[*] Test 2:\tOK\n"
+        # Execute -bd flag
+        build/binject -bd $BAD $PRM > /dev/null
+        if cmp -s $ESC $T3F
+        then :
+            printf "[*] Test 3:\tOK\n\n"
+            printf "[*] Check succeed\n"
+        else :
+            printf "[*] Test 3:\tFailed\n"
+        fi
     else :
         printf "[*] Test 2:\tFailed\n"
     fi
@@ -27,3 +43,5 @@ else :
 fi
 rm -f $T1F
 rm -f $T2F
+rm -f $XVL
+rm -f $ESC
